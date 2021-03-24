fe05e9f1c2e4fa7687f59934af6fbb99a3db5644
syahjamal <sulaimansyahjamal@gmail.com>
2021-03-24 18:06:37 +0700

    Migrasi dari template ke component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AsideMenu from './components/AsideMenu';
+import Browse from './components/Browse';
+import Client from './components/Client';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import JustArrive from './components/JustArrive';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Hero/>
+      <Browse/>
+      <JustArrive/>
+      <Client/>
+      <AsideMenu/>
+      <Footer/>
+    </>
   );
 }
 
