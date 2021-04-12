f35a580b583486ff6275d9b664a46db4eb4bf0e5
syahjamal <sulaimansyahjamal@gmail.com>
2021-04-12 10:14:08 +0700

    Add splash screen

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import AsideMenu from './components/AsideMenu';
-import Browse from './components/Browse';
-import Client from './components/Client';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import JustArrive from './components/JustArrive';
-import Offline from './components/Offline.js';
-
+import React from "react";
+import AsideMenu from "./components/AsideMenu";
+import Browse from "./components/Browse";
+import Client from "./components/Client";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import JustArrive from "./components/JustArrive";
+import Offline from "./components/Offline.js";
+import Splash from "./pages/Splash";
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [offlineStatus, setofflineStatus] = React.useState(!navigator.onLine);
+  const [isLoading, setisLoading] = React.useState(true);
 
   function handleOfflineStatus() {
     setofflineStatus(!navigator.onLine);
   }
 
-  React.useEffect(function(){
-      (async function(){
-        const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
-          headers:{
-            "Content-Type": "application/json",
-            "accept": "application/json",
-            "x-api-key": process.env.REACT_APP_APIKEY
+  React.useEffect(
+    function () {
+      (async function () {
+        const response = await fetch(
+          "https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              accept: "application/json",
+              "x-api-key": process.env.REACT_APP_APIKEY,
+            },
           }
-        });
+        );
         const { nodes } = await response.json();
         setItems(nodes);
 
@@ -34,29 +39,39 @@ function App() {
         script.async = false;
         document.body.appendChild(script);
       })();
-      
+
       handleOfflineStatus();
-      window.addEventListener('online', handleOfflineStatus);
-      window.addEventListener('offline', handleOfflineStatus);
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
+
+      setTimeout(function(){
+        setisLoading(false);
+      }, 1500)
 
       return function () {
-        window.removeEventListener('online', handleOfflineStatus);
-        window.removeEventListener('offline', handleOfflineStatus);
-      }
-  }, [offlineStatus]);
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
+    },
+    [offlineStatus]
+  );
 
   return (
     <>
-      {
-        offlineStatus && <Offline/>
-      }
-      <Header/>
-      <Hero/>
-      <Browse/>
-      <JustArrive items={items}/>
-      <Client/>
-      <AsideMenu/>
-      <Footer/>
+      {isLoading === true ? (
+        <Splash />
+      ) : (
+        <>
+          {offlineStatus && <Offline />}
+          <Header />
+          <Hero />
+          <Browse />
+          <JustArrive items={items} />
+          <Client />
+          <AsideMenu />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/pages/Splash.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from '../images/content/logo.png';
+
+function Splash() {
+  return (
+    <section>
+      <div className="container mx-auto min-h-screen">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="w-full md:w-4/12 text-center">
+            <img
+              src={logo}
+              alt="luxSpace | Fullfill your house with beautifull furniture"
+              className="mx-auto mb-8"
+            />
+            <p className="mb-16 px-4">
+              Kami menyediakan furniture berkelas yang membuat ruangan terasa
+              homey
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Splash;
